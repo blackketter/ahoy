b1fdebdf187b16e79dcc90090143e81cafa35a88
Dean Blackketter <dean@ahoy.us>
2014-01-29 08:59:45 -0800

    Buttons work on ATTiny48 proto board

--- package/ahoy/mcu-firmware/src/main.c
+++ package/ahoy/mcu-firmware/src/main.c
@@ -23,12 +23,23 @@ FUSES =
 #define MAX_BRIGHTNESS (99)
 #define PWM_STEPS (100)
 
+#define KEY_INPUT       PIND
 
-volatile uint8_t red = 0;
-volatile uint8_t green = 0;
-volatile uint8_t blue = 0;
+#define MAIN_BUTTON (1)
+#define UP_BUTTON (2)
+#define DOWN_BUTTON (4)
+
+
+volatile uint8_t red = 1;
+volatile uint8_t green = 1;
+volatile uint8_t blue = 1;
+
+volatile char key_state;                         // debounced and inverted key state: 
+                                                 // bit = 1: key pressed 
+volatile char key_press;                         // key press detect 
 
 // LED PWM interrupt routine
+// on 16-bit timer1
 ISR(TIMER1_COMPA_vect)
 {
   uint8_t color = 0;
@@ -47,40 +58,121 @@ ISR(TIMER1_COMPA_vect)
   PORTC = color;
 }
 
+// button interrupt service routine on 8-bit timer0
+ISR(TIMER0_OVF_vect)
+{
+  static char ct0, ct1; 
+  char i; 
+
+  i = key_state ^ ~KEY_INPUT;           // key changed ? 
+  ct0 = ~( ct0 & i );                   // reset or count ct0 
+  ct1 = ct0 ^ (ct1 & i);                // reset or count ct1 
+  i &= ct0 & ct1;                       // count until roll over ? 
+  key_state ^= i;                       // then toggle debounced state 
+                                        // now debouncing finished 
+  key_press |= key_state & i;           // 0->1: key press detect 
+}
+
+char get_key_press( char key_mask ) 
+{ 
+  cli(); 
+  key_mask &= key_press;                // read key(s) 
+  key_press ^= key_mask;                // clear key(s) 
+  sei(); 
+  return key_mask; 
+} 
+
 int main(void)
 {
-    DDRC = 7;           /* make the LED pin an output */
-    PORTC= 0; 
+  cli();             // disable global interrupts
 
-    // initialize Timer1
-    cli();             // disable global interrupts
-    TCCR1A = 0;        // set entire TCCR1A register to 0
-    TCCR1B = 0;
- 
- 
-    // set compare match register to desired timer count:
-    OCR1A = 65;
+//////////////////////////////////////////////////////////////////////////
+// Initialize LEDs
+//////////////////////////////////////////////////////////////////////////
+  DDRC = 7;           /* make the LED pin an output */
+  PORTC= 0; 
 
-    // enable timer compare interrupt:
-    TIMSK1 |= (1 << OCIE1A);
+  // initialize Timer1
+  TCCR1A = 0;        // set entire TCCR1A register to 0
+  TCCR1B = 0;
+
+
+  // set compare match register to desired timer count:
+  OCR1A = 65;
+
+  // enable timer compare interrupt:
+  TIMSK1 |= (1 << OCIE1A);
+  
+  // turn on CTC mode:
+  TCCR1B |= (1 << WGM12);
+  
+  // Full clock speed (no prescaling)
+  TCCR1B |= (1 << CS10);
+
+//////////////////////////////////////////////////////////////////////////
+// Initialize Buttons
+//////////////////////////////////////////////////////////////////////////
+
+  key_state = 0; 
+  key_press = 0; 
+  // initialize timer0
+  TCCR0A = (1<<CS02);          //divide by 256
+  TIMSK0 = 1<<TOIE0;                      //enable timer overflow interrupt 
+
+  DDRD = 0;   // all inputs for now
+  PORTD = 0xff;  // enablel pullups
+  
+  // enable global interrupts:
+  sei();
     
-    // turn on CTC mode:
-    TCCR1B |= (1 << WGM12);
+//////////////////////////////////////////////////////////////////////////
+// Main loop
+//////////////////////////////////////////////////////////////////////////
+
+  for(;;){
+    uint8_t buttons = get_key_press(0x07);   // read the lowest three bits
     
-    // Full clock speed (no prescaling)
-    TCCR1B |= (1 << CS10);
+    if (buttons & MAIN_BUTTON) {
+      if (red==0)
+      { 
+        red = 1; 
+       } else if (red == 0xff) {
+        red = 0;
+     } else {
+        red = (red << 1) + 1;
+      }
+    }
     
-    // enable global interrupts:
-    sei();
+    if (buttons & UP_BUTTON) {
+      if (green==0)
+      { 
+        green = 1; 
+      } else if (green == 0xff) {
+        green = 0;
+      } else {
+        green = (green << 1) + 1;
+      }
+    }
     
-    for(;;){
+    if (buttons & DOWN_BUTTON) {
+      if (blue==0)
+      { 
+        blue = 1; 
+      } else if (blue == 0xff) {
+        blue = 0;
+      } else {
+        blue = (blue << 1) + 1;
+      }
+    }
+
+/*        
       red--;
       red--;
       red--;
       green--;
       green--;
       blue--;
-      _delay_ms(4);
+*/
     }
 
     return 0;               /* never reached */
